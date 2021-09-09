2b073af1e7f74df3060e6c16b525fe39780e78b3
Sanketh Gunasekara <70938689+Sankethg0@users.noreply.github.com>
2021-09-10 00:27:15 +0530

    Done for the day

--- app/src/main/java/com/example/accountmanager/Login.java
+++ app/src/main/java/com/example/accountmanager/Login.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -12,6 +14,13 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final EditText email=findViewById(R.id.email);
+        final EditText password=findViewById(R.id.password);
+        final Button loginBTN=findViewById(R.id.signupBTN);
+
+       
+
     }
 
     public void gotoRegister(View view) {
--- app/src/main/java/com/example/accountmanager/Register.java
+++ app/src/main/java/com/example/accountmanager/Register.java
@@ -1,17 +1,76 @@
 package com.example.accountmanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Register extends AppCompatActivity {
+    //create object of the database reference class to access the firebase
+    DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReferenceFromUrl("https://account-manager-76ba4-default-rtdb.firebaseio.com/");
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText name=findViewById(R.id.name);
+        final EditText email=findViewById(R.id.email);
+        final EditText pass=findViewById(R.id.password);
+        final Button signupBTN=findViewById(R.id.signupBTN);
+
+        signupBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String nameTxt=name.getText().toString();
+                final String passTxt=pass.getText().toString();
+                final String emailTxt=email.getText().toString();
+
+
+                if(nameTxt.isEmpty()||passTxt.isEmpty()||emailTxt.isEmpty()){
+                    Toast.makeText(Register.this,"Fill all the details",Toast.LENGTH_SHORT).show();
+                }else{
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //Checks if the email is already available on the database
+                            if(snapshot.hasChild(emailTxt)){
+                                Toast.makeText(Register.this,"Email already Exists",Toast.LENGTH_SHORT).show();
+                            }else{
+                                //Sending data to firebase Realtime Database
+                                databaseReference.child("users").child(emailTxt).child("name").setValue(nameTxt);
+                                databaseReference.child("users").child(emailTxt).child("password").setValue(passTxt);
+
+                                Toast.makeText(Register.this,"User Registered",Toast.LENGTH_SHORT).show();
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
+                }
+            }
+        });
     }
 
     public void gotoLogin(View view) {
