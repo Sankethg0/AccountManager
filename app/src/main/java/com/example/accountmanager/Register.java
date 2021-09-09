72e10dab1668a3fa43585e25e9264387d6b3d797
Sanketh Gunasekara <70938689+Sankethg0@users.noreply.github.com>
2021-09-09 16:07:32 +0530

    login and register designed

--- app/src/main/java/com/example/accountmanager/Register.java
+++ app/src/main/java/com/example/accountmanager/Register.java
@@ -2,7 +2,9 @@ package com.example.accountmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+    }
+
+    public void gotoLogin(View view) {
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
